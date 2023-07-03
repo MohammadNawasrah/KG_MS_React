b086c5b62dfbf59f01d18e1525aa8879191418e9
Mohammad Al Nawasrah <nawasrahmohammad2000@gmail.com>
2023-07-04 01:54:24 +0300

    add login page design

---
+++ kg_react/src/component/login.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post('http://localhost:8080/login', { email, password });
+
+      if (response.data === 'Login successful') {
+        // Redirect to the success page
+        window.location.href = '/success-page'; // Replace with your actual success page URL
+      } else {
+        // Redirect to the failure page
+        window.location.href = '/failure-page'; // Replace with your actual failure page URL
+      }
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h2>Login</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={handleEmailChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={handlePasswordChange}
+            required
+          />
+        </div>
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
