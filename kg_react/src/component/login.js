0157ffa13bdc26cf9af16680bdf2d5a74616fce5
AhmadRaedNuman <ahmadnuman.dev@gmail.com>
2023-07-04 04:22:11 +0300

    modify login page and add successful and faild pages

--- kg_react/src/component/login.js
+++ kg_react/src/component/login.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+// import { useHistory } from 'react-router-dom';
 
 const LoginPage = () => {
-  const [email, setEmail] = useState('');
+  const [username, setusername] = useState('');
   const [password, setPassword] = useState('');
 
-  const handleEmailChange = (event) => {
-    setEmail(event.target.value);
+  const handleusernameChange = (event) => {
+    setusername(event.target.value);
   };
 
   const handlePasswordChange = (event) => {
@@ -17,14 +18,14 @@ const LoginPage = () => {
     event.preventDefault();
 
     try {
-      const response = await axios.post('http://localhost:8080/login', { email, password });
+      const response = await axios.post('http://localhost:8080/logins', { username, password });
 
       if (response.data === 'Login successful') {
-        // Redirect to the success page
-        window.location.href = '/success-page'; // Replace with your actual success page URL
+        // useHistory.push("./success_login.js");
+        window.location.href = './success_login.js'; // Replace with your actual success page URL
       } else {
-        // Redirect to the failure page
-        window.location.href = '/failure-page'; // Replace with your actual failure page URL
+        // useHistory.push("./failed_login.js");
+        window.location.href = './failed_login.js'; // Replace with your actual failure page URL
       }
     } catch (error) {
       console.log('Error:', error);
@@ -32,30 +33,32 @@ const LoginPage = () => {
   };
 
   return (
-    <div>
+    <div className="container">
       <h2>Login</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="email">Email:</label>
+        <div className="form-group">
+          <label htmlFor="username">username:</label>
           <input
-            type="email"
-            id="email"
-            value={email}
-            onChange={handleEmailChange}
+            type="text"
+            className="form-control"
+            id="username"
+            value={username}
+            onChange={handleusernameChange}
             required
           />
         </div>
-        <div>
+        <div className="form-group">
           <label htmlFor="password">Password:</label>
           <input
             type="password"
+            className="form-control"
             id="password"
             value={password}
             onChange={handlePasswordChange}
             required
           />
         </div>
-        <button type="submit">Login</button>
+        <button type="submit" className="btn btn-primary">Login</button>
       </form>
     </div>
   );
