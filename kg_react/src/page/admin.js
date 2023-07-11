7e133ad1960c24cfc21657ef054324c449e5a7b6
AhmadRaedNuman <ahmadnuman.dev@gmail.com>
2023-07-12 01:13:33 +0300

    add admin design

---
+++ kg_react/src/page/admin.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import "../static/css/admin.css";
+import Navbar from "../widget/navbar";
+const Admin = () => {
+    return (
+        <div className="container">
+            <Navbar />
+            <center><h3>administrator</h3></center>
+            <div className="card mb-3" id='card'>
+                <div className="card-body">
+                    <div className="d-flex flex-column flex-lg-row">
+                        <center>
+                            <h4>
+                                إدخال جميع الطلاب
+                            </h4>
+                            <div class="row">
+                                <input type="file" class="form-control" id="stdEXL" />
+                                <button type="button" class="btn btn-success" id='add'>إدخال الكل</button>
+                            </div>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div className="card mb-3" id='card'>
+                <div className="card-body">
+                    <div className="d-flex flex-column flex-lg-row">
+                        <center>
+                            <button type="button" class="btn btn-success" id='add'>معلومات الطلاب</button>
+                        </center>
+                    </div>
+                </div>
+            </div>
+            <div className="card mb-3" id='card'>
+                <div className="card-body">
+                    <div className="d-flex flex-column flex-lg-row">
+
+                        <center>
+                            <button type="button" class="btn btn-success" id='add'>إضافة معلم</button>
+                        </center>
+
+                    </div>
+                </div>
+            </div>
+            <div className="card mb-3" id='card'>
+                <div className="card-body">
+                    <div className="d-flex flex-column flex-lg-row">
+                        <button type="button" class="btn btn-success" id='add'>عرض المعلمين</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
+};
+export default Admin;
+
+
+
